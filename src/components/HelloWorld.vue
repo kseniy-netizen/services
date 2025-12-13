acb305fa3bc97ef1c5691a4ac33c6e90a153d4be
Kseniya <kseniya@example.com>
2025-12-13 18:30:47 +1000

    alt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,16 +19,6 @@ defineProps({
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
 
 .greetings h1,
 .greetings h3 {
